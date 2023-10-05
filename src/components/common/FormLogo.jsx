15bb15f19ca18a5bbb590842a0d683e57dbe8ef2
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-10-05 18:05:31 +0530

    Chaned logo

--- src/components/common/FormLogo.jsx
+++ src/components/common/FormLogo.jsx
@@ -1,9 +1,9 @@
-import Logo from "../../assets/images/logo.png";
+import Logo from "../../assets/images/LogoSide.png";
 
 export default function FormLogo() {
   return (
     <div className="text-center">
-      <img src={Logo} alt="logo" height={50} className="mb-3" />
+      <img src={Logo} alt="logo" height={100} className="mb-3" />
     </div>
   );
 }
