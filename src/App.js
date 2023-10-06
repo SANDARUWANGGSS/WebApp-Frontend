ac5ed6fadc989871fa10f1237eaae371a1a7e967
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-10-06 13:15:23 +0530

    updated app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Box, CssBaseline, LinearProgress } from "@mui/material";
+import { Box, CssBaseline, LinearProgress,createTheme,ThemeProvider } from "@mui/material";
 import "./App.css";
 import { Routes } from "./Routes";
 import { Suspense, useEffect, useMemo } from "react";
 import { SkeletonLoaderWithHeader } from "./components/common/SkeletonLoader";
 import { ToastProvider } from "./app/providers/toastProvider";
+import { APP_FONT_NAME } from "./resources/constants/StringConstants";
 
 export default function App() {
+  const MUI_THEME = createTheme({
+    typography: { fontFamily: APP_FONT_NAME },
+  });
   return (
     <div className="App">
+      <ThemeProvider theme={MUI_THEME}>
       <Router>
         <div>
           <ToastProvider>
@@ -23,6 +28,7 @@ export default function App() {
           </ToastProvider>
         </div>
       </Router>
+      </ThemeProvider>
     </div>
   );
 }
